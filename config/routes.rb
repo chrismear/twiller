e79f68f41f31ea39628597e8acbe2fff28d0ff06
Chris Mear <chris@feedmechocolate.com>
2008-06-21 12:36:55 +0100

    Add missing routes for activation and person state changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ ActionController::Routing::Routes.draw do |map|
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'people', :action => 'create'
   map.signup '/signup', :controller => 'people', :action => 'new'
-  map.resources :people
+  map.activate '/activate/:activation_code', :controller => 'people', :action => 'activate', :activation_code => nil
+  
+  map.resources :people, :member => {:suspend => :put, :unsuspend => :put, :purge => :delete}
 
   map.resource :session
 
